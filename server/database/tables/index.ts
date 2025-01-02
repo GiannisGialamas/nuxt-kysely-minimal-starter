df3364099354f116d2e37d1b351343cac1905637
GiannisGialamas <giannisgialamas1921@gmail.com>
2025-01-02 22:28:20 +0200

    refactor(db): fix exports

--- server/database/tables/index.ts
+++ server/database/tables/index.ts
@@ -1,11 +1,1 @@
-import { Generated, Insertable, Selectable, Updateable } from "kysely";
-
-export interface PostsTable {
-  id: Generated<number>;
-  title: string;
-  content: string;
-}
-
-export type Post = Selectable<PostsTable>;
-export type PostCreate = Insertable<PostsTable>;
-export type PostUpdate = Updateable<PostsTable>;
+export * from "./posts.table";
--- server/database/tables/posts.table.ts
+++ server/database/tables/posts.table.ts
@@ -1,1 +1,11 @@
-export * from "./posts.table";
+import { Generated, Insertable, Selectable, Updateable } from "kysely";
+
+export interface PostsTable {
+  id: Generated<number>;
+  title: string;
+  content: string;
+}
+
+export type Post = Selectable<PostsTable>;
+export type PostCreate = Insertable<PostsTable>;
+export type PostUpdate = Updateable<PostsTable>;
